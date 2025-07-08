3ed4b4419b74d86c772222dc4646dc73678c047f
Kayla <nyakukayla@gmail.com>
2025-07-08 19:31:15 -0400

    Ok

--- createTable.js
+++ createTable.js
@@ -1,6 +1,9 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+console.log("🔍 DATABASE_URL:", process.env.DATABASE_URL);
+
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
